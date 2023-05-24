eb63cff1d0ad9d0124561660b8f7542adbf0b1e5
ulfhiraa <studyiraa@gmail.com>
2023-05-24 11:25:51 +0700

    memasukkan data pelajar siswa 1

--- ParadigmaOOP/ParadigmaOOP.cpp
+++ ParadigmaOOP/ParadigmaOOP.cpp
@@ -39,3 +39,11 @@ public:
 	}
 };
 
+int main() {
+	pelajar siswa1("andi laksono", "SMAN 1 Bantul");
+	cout << siswa1.perkenalan();
+	cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+	return 0;
+}
+
