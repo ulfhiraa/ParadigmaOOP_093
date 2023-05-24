221ed59781125955075d802d3705c72909c77149
ulfhiraa <studyiraa@gmail.com>
2023-05-24 20:10:46 +0700

    class manusia memiliki 4 member : string (variabel name), objek jantung, manusia (constructor), ~manusia (destructor)

--- mainJM/Manusia.h
+++ mainJM/Manusia.h
@@ -4,4 +4,14 @@ using namespace std;
 
 class manusia {
 public:
+	string name;
+	jantung varJantung;
+
+	manusia(string pName);
+		: name(pName) {
+		cout << name << " hidup\n";
+	}
+	~manusia() {
+		cout << name << " mati\n";
+	}
 };
