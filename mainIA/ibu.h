c43b8cae4a3ee9ccd2744a36c013c220cdc8860f
ulfhiraa <studyiraa@gmail.com>
2023-05-24 19:30:42 +0700

    class ibu dengan 6 member. string (variabel nama), vector untuk menambahkan informasi dengan objek anak, ibu (constructor), ~ibu (destructor), void method untuk menambah anak dengan pointer anak, dan void method untuk menampilkan anak

--- mainIA/ibu.h
+++ mainIA/ibu.h
@@ -3,4 +3,15 @@
 
 class ibu {
 public:
+	string nama;
+	vector<anak*> daftar_anak;
+
+	ibu(string pNama) :nama(pNama) {
+		cout << "Ibu \"" << nama << "\" ada\n";
+	}
+	~ibu() {
+		cout << "Ibu \"" << nama << "\" tidak ada\n";
+	}
+	void tambahAnak(anak*);
+	void cetakAnak();
 };
