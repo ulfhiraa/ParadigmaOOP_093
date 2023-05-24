3220e2d3269b4587023fd2bdd74a6ba84912592b
ulfhiraa <studyiraa@gmail.com>
2023-05-24 14:32:56 +0700

    class pasien dengan 6 member. string (variabel nama), vector (objek pointer), pasien (constructor), ~pasien (destructor), void (method untuk menambahkan pointer dokter), void (method untuk menampilkan dokter)

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -5,4 +5,14 @@ using namespace std;
 class dokter;
 class pasien {
 public: 
+	string nama;
+	vector<dokter*> daftar_dokter;
+	pasien(string pNama) :nama(pNama) {
+		cout << "Pasien \"" << nama << "\" ada\n";
+	}
+	~pasien() {
+		cout << "Pasien \"" << nama << "\" tidak ada\n";
+	}
+	void tambahDokter(dokter*);
+	void cetakDokter();
 };
