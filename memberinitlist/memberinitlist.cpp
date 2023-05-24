b68c7848fa8d1afa30d06aad2578cba0030451fd
ulfhiraa <studyiraa@gmail.com>
2023-05-24 14:11:26 +0700

    class mahasiswa dengan 5 member const int (konstanta), string (variabel), float (nilai), mahasiswa (constructor), mahasiswa (destructor : melakukan sesuatu sebelum objek berakhir, untuk menampilkan program)

--- memberinitlist/memberinitlist.cpp
+++ memberinitlist/memberinitlist.cpp
@@ -3,5 +3,19 @@
 using namespace std;
 
 class mahasiswa {
-	private 
+private:
+	const int id;
+	string nama;
+	float nilai;
+
+public:
+	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) { //Member Initial List
+	   //Definisi
+	}
+
+	~mahasiswa() {
+		cout << "Id    = " << id << endl;
+		cout << "Nama  = " << nama << endl;
+		cout << "Nilai = " << nilai << endl;
+	}
 };
