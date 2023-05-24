1e8e41c494caf3158e6d1d6f9589fc3c22b052d5
ulfhiraa <studyiraa@gmail.com>
2023-05-24 19:51:50 +0700

    keyword "new" ibu dan anak untuk alokasi atau meminta memori pada cpu pengguna program

--- mainIA/mainIA.cpp
+++ mainIA/mainIA.cpp
@@ -5,4 +5,11 @@ using namespace std;
 #include "anak.h"
 #include "ibu.h"
 
+int main() {
+	ibu* varIbu = new ibu("dini");
+	ibu* varIbu2 = new ibu("novi");
+	anak* varAnak1 = new anak("tono");
+	anak* varAnak2 = new anak("rini");
+	anak* varAnak3 = new anak("Dewi");
 
+}
