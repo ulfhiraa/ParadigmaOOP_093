8d563e658b99e23582efed5b1ee783ff0aaa032f
ulfhiraa <studyiraa@gmail.com>
2023-05-24 11:06:13 +0700

    class orang dengan 4 member

--- ParadigmaOOP/ParadigmaOOP.cpp
+++ ParadigmaOOP/ParadigmaOOP.cpp
@@ -4,5 +4,18 @@ using namespace std;
 
 class orang {
 public: 
-	
+	string nama;
+
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "orang dibuat\n" << endl;
+	}
+
+	~orang() {
+		cout << "orang dihapus\n" << endl;
+	}
+
+	int jumlah(int a, int b) {
+		return a + b;
+	}
 };
